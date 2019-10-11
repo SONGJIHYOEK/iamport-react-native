d2aa6a730d2a4f98774bd144a39557c67acaaf74
SONGJIHYEOK <porr3444@naver.com>
2019-10-11 19:48:02 +0900

    Classify

--- exampleForWebView/src/index.js
+++ exampleForWebView/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import Home from './Home';
-import Payment from './Payment';
+import Home from './HomeAsClass';
+import Payment from './PaymentAsClass';
 import Certification from './Certification';
 
 const AppNavigator = createStackNavigator({
